a9c897a551391114ca2790a597c4bbef1ff1c2b7
Sadia-Islam-Shafina <sishafina@gmail.com>
2024-09-02 16:13:55 +0600

    change the text title and add table

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,8 +3,13 @@ import React from 'react'
 export const Home = () => {
   return (
     <div>
-      <h1>jiiiiiiiiiiiiiiiiiiiiiiiii</h1>
-
+      <h1>My name is sadia!</h1>
+      <table>
+        <tr>
+          <td>adasd</td>
+          <td>adasd</td>
+        </tr>
+      </table>
     </div>
   )
 }
